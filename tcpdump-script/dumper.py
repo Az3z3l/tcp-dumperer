f06c9f0e55acee7d125cfc33a017e45d3ee47d56
Yaswant <52544332+Az3z3l@users.noreply.github.com>
2020-08-10 11:34:51 +0530

    Update dumper.py

--- tcpdump-script/dumper.py
+++ tcpdump-script/dumper.py
@@ -2,11 +2,12 @@ import requests
 import os
 
 urla="http://34.68.14.102"
+timer=300
 url=urla+"/uploader"
 
 file_number=0
 while(True):
-    os.system('sudo timeout 300 tcpdump -w '+str(file_number)+'.pcap -i any')
+    os.system('sudo timeout '+str(timer)+' tcpdump -w '+str(file_number)+'.pcap -i any')
     files = {'file': open(str(file_number)+'.pcap', 'rb')}
     getdata = requests.post(url, files=files)
     os.system('sudo rm '+str(file_number)+'.pcap')
